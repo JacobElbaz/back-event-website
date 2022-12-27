dd5eeaa7f9b1b8ce975f066b689ae52a505735b7
JacobElbaz <jacobel@post.bgu.ac.il>
2022-12-27 20:37:01 +0200

    config

--- src/index.js
+++ src/index.js
@@ -12,12 +12,6 @@ app.use(express.json());
 app.use("/guests", routes);
 dotenv.config();
 
-const public_path = path.join(__dirname, '../build');
-app.use(express.static(public_path));
-app.get('*', (_, res) => {
-    res.sendFile(path.join(public_path, 'index.html'));
-})
-
 const uri = process.env.STRING_URI;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
